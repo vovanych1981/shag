822deb7ee820b1dacf6337d471e50a3d9690cd53
vovanych1981 <101288760+vovanych1981@users.noreply.github.com>
2022-06-28 20:51:38 +0300

    lesson1

---
+++ Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+//        Дана сторона квадрата a. Найти его периметр P = 4·a
+//        int a = 5;
+//        int P = a * 4;
+//        System.out.println(P);
+//        Дана сторона квадрата a. Найти его площадь S = a  ** 2
+
+//        int a = 8;
+//        int S = a * a;
+//        System.out.println("У квадрата со стороной " + a + " площадь " + S);
+
+//        Даны стороны прямоугольника a и b. Найти его площадь S = a·b и
+//        периметр P = 2·(a + b)
+//
+//        Дан диаметр окружности d. Найти ее длину L = π·d. В качестве
+//        значения π использовать 3.14
+//        int d = 10;
+//        final double pi = 3.14;
+//        double L = pi * d;
+//        System.out.println("У круга с диаметром " + d + " длина окружности " + L);
+
+//        Дана длина ребра куба a. Найти объем куба V = a ** 3 и площадь его    поверхности S = 6·a**2
+
+//        int a = 5;
+//        int V = a * a * a;
+//        int S = 6 * a * a;
+//        System.out.println("Объем куба " + V);
+//        System.out.println("Площадь поверхности куба " + S);
+
+//        Даны длины ребер a, b, c прямоугольного параллелепипеда. Найти
+//        его объем V = a·b·c и площадь поверхности S = 2·(a·b + b·c + a·c).
+
+//        int a = 2;
+//        int b = 3;
+//        int c = 4;
+//        int V = a*b*c;
+//        System.out.println("");
+
+//        Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2
+
+        int a = 2;
+        int b = 3;
+        double z = (a + b) / 2.0;
+        System.out.println(z);
+
+
+    }
+}
