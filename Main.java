9e5024ea3cc3b6a4e982e5986a5cf7c406e11d82
vovanych1981 <101288760+vovanych1981@users.noreply.github.com>
2022-06-28 21:49:43 +0300

    lesson1

--- Main.java
+++ Main.java
@@ -42,11 +42,86 @@ public class Main {
 //        System.out.println("");
 
 //        Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2
+//
+//        int a = 2;
+//        int b = 3;
+//        double z = (a + b) / 2.0;
+//        System.out.println(z);
 
-        int a = 2;
-        int b = 3;
-        double z = (a + b) / 2.0;
-        System.out.println(z);
+//        Даны два неотрицательных числа a и b. Найти их среднее геометрическое, то есть квадратный корень из их
+//        произведения: √ a·b.
+//        double a = 3.45;
+//        double b = 7.2;
+//        double g_avg = Math.sqrt(a*b);
+//        System.out.println(g_avg);
+
+//        Даны два ненулевых числа. Найти сумму, разность, произведение и
+//        частное их квадратов.
+
+//        int a = 2;
+//        int b = 3;
+//        int c = a * a + b * b;
+//        int d = a * a - b * b;
+//        int e = (a * a) * (b * b);
+//        double f = (a * a) / (double)(b * b);
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(e);
+//        System.out.println(f);
+
+//        Даны два ненулевых числа. Найти сумму, разность, произведение и
+//        частное их модулей.
+//
+//        int a = -5;
+//        int b = 24;
+//        a = Math.abs(a);
+//        b = Math.abs(b);
+//        double s = a+b;
+//        System.out.println("Сумма модулей " + (int)s);
+//        s = a-b;
+//        System.out.println("Разность модулей " + (int)s);
+//        s = a*b;
+//        System.out.println("Произведение модулей " + (int)s);
+//        s = (double)a/b;
+//        System.out.println("Частное модулей " + s);
+
+//        Поменять местами содержимое переменных A и B и вывести новые
+//        значения A и B.
+
+//        int a = 3;
+//        int b = 10;
+//        int c = a;
+//        a = b;
+//        b = c;
+//
+//        System.out.println(a+b);
+
+//        Даны катеты прямоугольного треугольника a и b. Найти его гипотенузу c и периметр P:
+//        c =√ a**2 + b**2    , P = a + b + c
+
+//        int a = 2;
+//        int b = 4;
+//        double c = Math.sqrt((a * a) + (b * b));
+//        System.out.println(c);
+//        double P = a + b + c;
+//        System.out.println(P);
+//
+//        Даны два круга с общим центром и радиусами R1 и R2 (R1 > R2).
+//                Найти площади этих кругов S1 и S2, а также площадь S3 кольца, внешний
+//        радиус которого равен R1, а внутренний радиус равен R2:
+//        S1 = π·(R1)**2 , S2 = π·(R2) **2 , S3 = S1 − S2.
+//                В качестве значения π использовать 3.14.
+
+        int R1 = 20;
+        int R2 = 15;
+        final double pi = 3.14;
+        double s1 = pi (R1 * R1);
+        double s2 = pi (R2 * R2);
+        double s3 = s1 - s2;
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
 
 
     }
