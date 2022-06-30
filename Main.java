171f373e87873c1483f80906f465e2dde9cdc152
vovanych1981 <101288760+vovanych1981@users.noreply.github.com>
2022-06-30 21:15:43 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,128 +1,127 @@
-package com.company;
+package lesson2;
+
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        // write your code here
-//        Дана сторона квадрата a. Найти его периметр P = 4·a
-//        int a = 5;
-//        int P = a * 4;
-//        System.out.println(P);
-//        Дана сторона квадрата a. Найти его площадь S = a  ** 2
+//        Integer1◦
+//. Дано расстояние L в сантиметрах. Используя операцию деления
+//        нацело, найти количество полных метров в нем (1 метр = 100 см).
 
-//        int a = 8;
-//        int S = a * a;
-//        System.out.println("У квадрата со стороной " + a + " площадь " + S);
-
-//        Даны стороны прямоугольника a и b. Найти его площадь S = a·b и
-//        периметр P = 2·(a + b)
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите целое число - к-во в сантиметрах: ");
+//        int L = in.nextInt();
+//        System.out.println(L/100 + " метров");
 //
-//        Дан диаметр окружности d. Найти ее длину L = π·d. В качестве
-//        значения π использовать 3.14
-//        int d = 10;
-//        final double pi = 3.14;
-//        double L = pi * d;
-//        System.out.println("У круга с диаметром " + d + " длина окружности " + L);
-
-//        Дана длина ребра куба a. Найти объем куба V = a ** 3 и площадь его    поверхности S = 6·a**2
-
-//        int a = 5;
-//        int V = a * a * a;
-//        int S = 6 * a * a;
-//        System.out.println("Объем куба " + V);
-//        System.out.println("Площадь поверхности куба " + S);
-
-//        Даны длины ребер a, b, c прямоугольного параллелепипеда. Найти
-//        его объем V = a·b·c и площадь поверхности S = 2·(a·b + b·c + a·c).
-
-//        int a = 2;
-//        int b = 3;
-//        int c = 4;
-//        int V = a*b*c;
-//        System.out.println("");
-
-//        Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2
 //
-//        int a = 2;
-//        int b = 3;
-//        double z = (a + b) / 2.0;
-//        System.out.println(z);
-
-//        Даны два неотрицательных числа a и b. Найти их среднее геометрическое, то есть квадратный корень из их
-//        произведения: √ a·b.
-//        double a = 3.45;
-//        double b = 7.2;
-//        double g_avg = Math.sqrt(a*b);
-//        System.out.println(g_avg);
-
-//        Даны два ненулевых числа. Найти сумму, разность, произведение и
-//        частное их квадратов.
-
-//        int a = 2;
-//        int b = 3;
-//        int c = a * a + b * b;
-//        int d = a * a - b * b;
-//        int e = (a * a) * (b * b);
-//        double f = (a * a) / (double)(b * b);
-//        System.out.println(c);
-//        System.out.println(d);
-//        System.out.println(e);
-//        System.out.println(f);
-
-//        Даны два ненулевых числа. Найти сумму, разность, произведение и
-//        частное их модулей.
+//        Integer2◦
+//. Дана масса M в килограммах. Используя операцию деления нацело,
+//        найти количество полных тонн в ней (1 тонна = 1000 кг).
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите целое число - к-во килограммов: ");
+//        int K = in.nextInt();
+//        System.out.println(K/1000 + " тонн");
 //
-//        int a = -5;
-//        int b = 24;
-//        a = Math.abs(a);
-//        b = Math.abs(b);
-//        double s = a+b;
-//        System.out.println("Сумма модулей " + (int)s);
-//        s = a-b;
-//        System.out.println("Разность модулей " + (int)s);
-//        s = a*b;
-//        System.out.println("Произведение модулей " + (int)s);
-//        s = (double)a/b;
-//        System.out.println("Частное модулей " + s);
-
-//        Поменять местами содержимое переменных A и B и вывести новые
-//        значения A и B.
-
-//        int a = 3;
-//        int b = 10;
-//        int c = a;
-//        a = b;
-//        b = c;
+////        Integer3◦
+//. Дан размер файла в байтах. Используя операцию деления нацело,
+//        найти количество полных килобайтов, которые занимает данный файл
+////                (1 килобайт = 1024 байта).
+//        Scanner in = new Scanner(System.in);
+//        int byute
 //
-//        System.out.println(a+b);
+//        Integer6◦
+//. Дано двузначное число. Вывести вначале его левую цифру (десятки), а затем — его правую цифру (единицы). Для нахождения десятков
+//        использовать операцию деления нацело, для нахождения единиц — операцию взятия остатка от деления.
 
-//        Даны катеты прямоугольного треугольника a и b. Найти его гипотенузу c и периметр P:
-//        c =√ a**2 + b**2    , P = a + b + c
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите двузначное число: ");
+//        int D = in.nextInt();
+//        System.out.println(D/10 + " - это десятки");
+//        System.out.println(D%10 + " - это единицы");
 
-//        int a = 2;
-//        int b = 4;
-//        double c = Math.sqrt((a * a) + (b * b));
-//        System.out.println(c);
-//        double P = a + b + c;
-//        System.out.println(P);
+//        Integer7◦
+//. Дано двузначное число. Найти сумму и произведение его цифр.
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите двузначное число: ");
+//        int D = in.nextInt();
+//        int a = D/10;
+//        int b = D%10;
+//        System.out.println((a+b) + " - это сумма");
+//        System.out.println((a*b) + " - это произведение");
+
+//        Integer8◦
+//. Дано двузначное число. Вывести число, полученное при перестановке цифр исходного числа.
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите двузначное число: ");
+//        int number = in.nextInt();
+//        int chislo1 = number/10;
+//        int chislo2 = number%10;
+//        int x = chislo2 * 10 + chislo1;
 //
-//        Даны два круга с общим центром и радиусами R1 и R2 (R1 > R2).
-//                Найти площади этих кругов S1 и S2, а также площадь S3 кольца, внешний
-//        радиус которого равен R1, а внутренний радиус равен R2:
-//        S1 = π·(R1)**2 , S2 = π·(R2) **2 , S3 = S1 − S2.
-//                В качестве значения π использовать 3.14.
+//        System.out.println(x + " Это новое число");
 
-        int R1 = 20;
-        int R2 = 15;
-        final double pi = 3.14;
-        double s1 = pi (R1 * R1);
-        double s2 = pi (R2 * R2);
-        double s3 = s1 - s2;
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s3);
+//        Integer9◦
+//. Дано трехзначное число. Используя одну операцию деления нацело,
+//  вывести первую цифру данного числа (сотни).
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите трехзначное число: ");
+//        int number = in.nextInt();
+//        System.out.println("Первая цифра равняется - " + number/100);
+
+//        Integer10◦
+//. Дано трехзначное число. Вывести вначале его последнюю цифру
+// (единицы), а затем — его среднюю цифру (десятки).
+
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите трехзначное число: ");
+//        int number = in.nextInt();
+//        System.out.println("Единицы - " + number%10);
+//        int des = (number/10) % 10; // (number%100) / 10;
+//        System.out.println("Единицы - " + des);
+
+//        Integer11◦
+//. Дано трехзначное число. Найти сумму и произведение его цифр.
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите трехзначное число: ");
+//        int number = in.nextInt();
+//        int edinicy = number%10;
+//        int des = (number / 10) % 10;
+//        int sotni = number / 100;
+//        System.out.println("Сумма: " + (edinicy+des+sotni));
+//        System.out.println("Произведение: " + (edinicy*des*sotni));
+
+//        Integer12◦
+//. Дано трехзначное число. Вывести число, полученное при прочтении исходного числа справа налево.
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите трехзначное число: ");
+//        int number = in.nextInt();
+//        int edinicy = number%10;
+//        int des = (number / 10) % 10;
+//        int sotni = number / 100;
+////        System.out.printf("%s%s%s\n", edinicy, des, sotni);
+//        int res = 100*edinicy + 10*des + sotni;
+//        System.out.println(res);
+
+//        Integer13◦
+//. Дано трехзначное число. В нем зачеркнули первую слева цифру и
+//  приписали ее справа. Вывести полученное число.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите трехзначное число: ");
+        int number = in.nextInt();
+        int ed = number%100;
+        int sotni = number / 100;
+        int nov_chislo = ed*10 + sotni;
+        System.out.println(nov_chislo);
+
+
+
 
 
 
     }
+
 }
