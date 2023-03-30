2a3ce2875280d8a2765c5d6fe4d03a510324743b
vovanych1981 <101288760+vovanych1981@users.noreply.github.com>
2023-03-30 21:21:12 +0300

    Form

---
+++ Lesson17 JS/lesson17 — копия.js
@@ -0,0 +1,29 @@
+#logForm{
+    background-color:blue;
+    display:block;
+    height:160px;
+    padding:20px;
+    width:400px;
+    }
+
+#formText{
+    color:white;
+    float:left;
+    font-size:40px;
+    font-weight:bold;
+    height:200px;
+    width:200px;
+    }
+
+.rounded{
+    border-radius:15px;
+    border-width:0;
+    display:block;
+    height:30px;
+    margin:10px 0;
+    padding:0;
+    width:170px;
+    }
+
+  
+  
